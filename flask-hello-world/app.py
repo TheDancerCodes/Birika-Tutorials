672dff3930a434ed8f4ade92ecb0a45ada2cd654
Roger Taracha <rojtaracha@gmail.com>
2017-03-01 17:31:00 +0300

    Dynamic Routes:   + Added dynamic routes with status codes.   + Incorporated DEBUG mode.   + Added gitignore file.

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,13 +6,43 @@ from flask import Flask
 #create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # Use decorators to link the function to the url
 @app.route("/")
 @app.route("/hello")
 
 # Define the view using a function, which returns a string.
 def hello_world():
-    return "Hello World!"
+    return "Hello World!?!?!?!?!?"
+
+# dynamic routes: route takes a query parameter.
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "Correct!"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1
+    return "Correct!"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    return "Correct!"
+
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael" :
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
