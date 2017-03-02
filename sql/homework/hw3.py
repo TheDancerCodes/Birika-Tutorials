24e34cf2bf04c7a8c7b4b138fe9a81c80b8066cd
Roger Taracha <rojtaracha@gmail.com>
2017-03-02 22:29:40 +0300

    Homework 3

---
+++ sql/homework/hw3.py
@@ -0,0 +1,28 @@
+# UPDATE statement
+
+# Update the quantity on two of the records, and then output all of the records
+# from the table.
+
+# Finally output only records that are for Ford vehicles.
+
+
+import sqlite3
+
+with sqlite3.connect("nganya.db") as connection:
+    c = connection.cursor()
+
+    c.execute("UPDATE cars SET Quantity = 32 WHERE \
+        Model = 'Mustang'")
+
+    # update data
+    c.execute("UPDATE cars SET Quantity = 26 WHERE \
+        Model = 'Fiesta'")
+
+    print "\nNEW DATA:\n"
+
+    c.execute("SELECT * FROM cars")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print (r[0], r[1], r[2])
