343eb72713c1cd03706adafa3a1b1a0e4fb63105
Roger Taracha <rojtaracha@gmail.com>
2017-03-06 15:55:31 +0300

    SQL Functions   + AVG()   + COUNT()   + MAX()   + MIN()   + SUM()

---
+++ sql/sqlm.py
@@ -0,0 +1,25 @@
+#SQLite Functions
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    # create a dictionary of sql
+    sql = {'average': "SELECT avg(population) FROM population",
+           'maximum': "SELECT max(population) FROM population",
+           'minimum': "SELECT min(population) FROM population",
+           'sum': "SELECT sum(population) FROM population",
+           'count': "SELECT count(population) FROM population"}
+
+    # run each sql query item in the directory
+    for keys, values in sql.iteritems():
+
+        # run sql
+        c.execute(values)
+
+        # fetchone() retrieves one record from the query
+        result = c.fetchone()
+
+        # output the result to the screen
+        print keys + ":", result[0]
