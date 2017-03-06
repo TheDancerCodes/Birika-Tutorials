4a72065e8c4d3f32cadc1a748e9af1e0ce34751e
Roger Taracha <rojtaracha@gmail.com>
2017-03-06 20:31:55 +0300

    User Login:   + Updated Configuration.   + Added form to login.html   + Updated login function & Added a logout function [ blog.py ]

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -8,6 +8,9 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hot_cocoa_inna_di_flask'
 
 app = Flask(__name__)
 
@@ -18,13 +21,31 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    """
+        The logout() function uses the pop() method to reset the session key
+        to the default value when the user logs out.
+    """
+    session.pop('logged_in', None)
+    flash('You were logged out!')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
