c264f9b84f67cdd93275fac07fc50bbc8e115bef
Roger Taracha <rojtaracha@gmail.com>
2017-03-06 21:32:42 +0300

    Sessions and Login_required Decorator:   + The login_required decorator checks to make sure that a user is authorized (e.g. logged_in ) before allowing access to certain pages.

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,8 +1,12 @@
 # blog.py - controller
 
+# Functools is a module used for extending the capabilities of functions with
+# other functions, which is exactly what decorators accomplish.
+
 # imports
 from flask import Flask, render_template, request, session, flash, redirect, \
 url_for, g
+from functools import wraps
 
 import sqlite3
 
@@ -21,6 +25,17 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
@@ -34,6 +49,7 @@ def login():
     return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
