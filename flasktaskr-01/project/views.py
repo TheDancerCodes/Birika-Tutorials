5ec00d42d36c39ba3ee887f4feb114eb070b1f18
Roger Taracha <rojtaracha@gmail.com>
2017-03-18 13:32:22 +0300

    Tasks:   + Querying the database for open and closed tasks (read)   +  assigning the results to two variables, open_tasks & closed_tasks   + and then passing those variables to the tasks.html

--- flasktaskr-01/project/views.py
+++ flasktaskr-01/project/views.py
@@ -4,7 +4,7 @@ import sqlite3
 from functools import wraps
 
 from flask import Flask, flash, redirect, render_template, request, session, \
-    url_for
+    url_for, g
 
 
 # config
@@ -49,3 +49,30 @@ def login():
             flash('Welcome!')
             return redirect(url_for('tasks'))
     return render_template('login.html')
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cur = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=1'
+    )
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+             task_id=row[3]) for row in cur.fetchall()
+    ]
+    cur = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=0'
+    )
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+             task_id=row[3]) for row in cur.fetchall()
+    ]
+    g.db.close()
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
